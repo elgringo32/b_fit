893a7352a07864d6077b9f632c79a3daf7684831
Brian Knopke <BrianK@EDIT-2.local>
2022-06-09 09:36:08 -0700

    added DB, ejs, body-parser, and exercise post

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+const cors = require('cors');
+var bodyParser = require('body-parser')
+require('dotenv').config()
 
-app.get('/',(req, res) => {
-    res.sendFile(__dirname + '/index.html')
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'bfit_workout'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+    .catch(error => console.error(error))
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/',(request, response)=>{
+        response.render('index.ejs')
+})
+
+app.post('/api/addExercise',(request, response)=>{
+    // db.collection('exercises').inse
+    console.log(request.body)
+    response.redirect('/')
+
 })
 
 app.listen(3000, function() {
